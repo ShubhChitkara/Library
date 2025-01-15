9d02861e209461bba1eb469875f75e125d1ae1fe
ShubhChitkara <186718633+ShubhChitkara@users.noreply.github.com.>
2025-01-15 18:28:30 +0000

    Used the dialog tag

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Library
+THINGS TO DO
+set dialog to center of the screen
+manage return values of the form 
+convert those return values to object 
+add the object to myLibrary
+show each object into a grid in the middle
--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 const myLibrary = [];
-const addabookbtn=document.querySelector(".addabook");
-addabookbtn.addEventListener("click",addBookToLibrary(book));
+const dialog = document.querySelector("dialog");
+const addabookbtn = document.querySelector(".addabook");
+addabookbtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+const closeButton = document.querySelector(".closemodalbtn");
+//form submission
+closeButton.addEventListener("click", (event) => {
+    event.preventDefault(); // We don't want to submit this fake form
+    dialog.close(selectEl.value); // Have to send the select box value here.
+  });
+
 //combines the book data into object
-function createBook(booktitle,bookauthor,booklength,bookreadstatus){
-    const book={
-        title:booktitle,
-        author:bookauthor,
-        noofpages:booklength,
-        readstatus:bookreadstatus
-    };
+function createBook(booktitle, bookauthor, booklength, bookreadstatus) {
+  const book = {
+    title: booktitle,
+    author: bookauthor,
+    noofpages: booklength,
+    readstatus: bookreadstatus,
+  };
 }
 //form that opens when you press add a book button
-function bookdetails(){
-    const forms=document.createElement("form");
-    const name=forms.createElement("input");
-    name.type="text";
-
-
-//call function createBook in this function and then call function addBookToLibrary 
+function bookdetails() {
+  //call function createBook in this function and then call function addBookToLibrary
 }
 //adds the book object to array
 function addBookToLibrary(book) {
-    myLibrary.push(book);
-   
+  myLibrary.push(book);
 }
-  
+console.log(myLibrary);
 
-console.log(myLibrary)
