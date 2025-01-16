124ec6106bd00658648f57e8c1b1eda745b808ec
ShubhChitkara <186718633+ShubhChitkara@users.noreply.github.com.>
2025-01-16 05:31:58 +0000

    Removed some bugs

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # Library
-THINGS TO DO
-manage return values of the form 
-convert those return values to object 
-add the object to myLibrary
+THINGS TO DO 
 show each object into a grid in the middle
--- server.js
+++ server.js
@@ -1,50 +1,55 @@
 const myLibrary = [];
-const addBookButton = document.querySelector('.addabook');
-const dialog = document.querySelector('dialog');
-const submitForm = document.querySelector('form');
+const addBookButton = document.querySelector(".addabook");
+const dialog = document.querySelector("dialog");
+const submitForm = document.querySelector("form");
 
+//Creates the book object
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read
-
-    this.info = function(){
-        let result = '';
-        if(this.read == 'yes'){
-            return result = `${title} by ${author}, ${pages} pages, read it`;
-        }else{
-            return result = `${title} by ${author}, ${pages} pages, not read yet`;
-        }
+  this.info = function () {
+    let result = "";
+    if (this.read == "yes") {
+      return (result = `${title} by ${author}, ${pages} pages, read it`);
+    } else {
+      return (result = `${title} by ${author}, ${pages} pages, not read yet`);
     }
+  };
 }
 
 //form that opens when you press add a book button
-addBookButton.addEventListener('click',() => {
+addBookButton.addEventListener("click", () => {
   dialog.showModal();
 });
 //Adds book to library
-function addBookToLibrary(title, author, pages, read){
+function addBookToLibrary(title, author, pages, read) {
   let book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
+
 //Adding inputted book to library
-submitForm.addEventListener('submit', function(e){
+function doit() {
+  let title = document.querySelector("#title");
+  let author = document.querySelector("#author");
+  let pages = document.querySelector("#pages");
+
+  if (document.querySelector("#read").checked) {
+    let read = document.querySelector("#read");
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
+    dialog.close();
+    submitForm.reset();
+  } else {
+    let read = document.querySelector("#not-read");
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
+    dialog.close();
+    submitForm.reset();
+  }
+}
+submitForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  let title = document.querySelector('#title');
-  let author = document.querySelector('#author');
-  let pages = document.querySelector('#pages');
-  
-  if(document.querySelector("#read").checked){
-      let read = document.querySelector('#read');
-      addBookToLibrary(title.value, author.value, pages.value, read.value);
-      dialog.close();
-      submitForm.reset();
-  }
-  else {
-      let read = document.querySelector('#not-read');
-      addBookToLibrary(title.value, author.value, pages.value, read.value);
-      dialog.close();
-      submitForm.reset();
-  }
+  doit();
+});
+console.log(myLibrary)
